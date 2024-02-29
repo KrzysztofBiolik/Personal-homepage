ea2c65b2a8a2582c289183a8acb6e34e1972e26e
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-02-29 15:40:13 +0100

    Prepared colors for ligtht and dark theme

--- src/App/theme.js
+++ src/App/theme.js
@@ -1,1 +1,35 @@
-export const theme = {};
+const colorNames = {
+    white: "#FFFFFF",
+    whiteLilac: "#FBFBFE",
+    slateGray: "#6E7E91",
+    doveGray: "#6D6D6D",
+    mercury: "#e5e5e5",
+    mineShaft: "#252525",
+
+};
+
+export const themeLight = {
+    colors: {
+        site: {
+            background: colorNames.whiteLilac,
+            text: colorNames.slateGray,
+        },
+        themeSwitch: {
+            background: colorNames.mercury,
+            icon: colorNames.white,
+        }
+    },
+};
+
+export const themeDark = {
+    colors: {
+        site: {
+            background: colorNames.mineShaft,
+            text: colorNames.white,
+        },
+        themeSwitch: {
+            background: colorNames.doveGray,
+            icon: colorNames.mineShaft,
+        }
+    },
+};
