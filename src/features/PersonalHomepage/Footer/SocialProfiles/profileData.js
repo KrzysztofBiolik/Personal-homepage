9707ecebb74347e1c0aabb84cc714b8269a4eeff
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-03-04 13:05:54 +0100

    Created file with profiles data

---
+++ src/features/PersonalHomepage/Footer/SocialProfiles/profileData.js
@@ -0,0 +1,22 @@
+import { ReactComponent as GithubIon } from "./SocialIcons/Github.svg";
+import { ReactComponent as Facebook } from "./SocialIcons/Facebook.svg";
+import { ReactComponent as Instagram } from "./SocialIcons/Instagram.svg";
+
+
+export const profilesData = [
+    {
+        name: "GitHub",
+        url: "https://github.com/KrzysztofBiolik",
+        Icon: GithubIon,
+    },
+    {
+        name: "Facebook",
+        url: "https://www.facebook.com/profile.php?id=61552096169036&locale=pl_PL",
+        Icon: Facebook,
+    },
+    {
+        name: "Instagram",
+        url: "https://www.instagram.com/krzysiek_biolik/",
+        Icon: Instagram,
+    },
+];
