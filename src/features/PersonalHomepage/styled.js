936ec89e65656b568c15ff7f8758232af76cb6a6
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-03-04 12:17:37 +0100

    Added margin, padding and max-width to the site

--- src/features/PersonalHomepage/styled.js
+++ src/features/PersonalHomepage/styled.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    margin: 0 auto;
-    max-width:900px;
+    margin: auto;
+    max-width:1248px;
+    padding:16px;
 `;
