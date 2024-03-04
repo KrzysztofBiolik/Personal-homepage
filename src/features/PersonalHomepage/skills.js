157bbe6b016cff58554f853cd79dd2602917cb27
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-03-04 12:12:29 +0100

    Added skills arrays file to Skill component

---
+++ src/features/PersonalHomepage/skills.js
@@ -0,0 +1,22 @@
+export const skills = [
+    "Semantic & accessible HTML",
+    "Css Flexbox",
+    "Css Grid",
+    "Css BEM convention",
+    "JavaScript: ES6+",
+    "Markdown",
+    "React",
+    "React-Redux",
+    "React Hooks",
+    "GitHub Pull Requests and Review",
+    "Responsive Web Design",
+    "Git",
+    "Immutability",
+    "Debbuging",
+];
+
+export const nextSkills = [
+    "TypeScript",
+    "Node.js",
+    "JS Classes",
+];
