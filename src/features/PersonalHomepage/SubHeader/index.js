e4af2963057bbbf1f17c5b4e0ad206d705125110
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-03-05 14:54:44 +0100

    Created Subheader component

---
+++ src/features/PersonalHomepage/SubHeader/index.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const SubHeader = styled.h2`
+    font-size: 30px;
+    margin:0;
+    font-weight: 900;
+    color: ${({theme}) => theme.colors.text};
+`;
