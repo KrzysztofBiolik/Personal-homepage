e64abd2a3654e4b0ba7bd9e3e6fbb64f7bf72743
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-03-05 12:40:44 +0100

    Styled SocialProfiles component

--- src/features/PersonalHomepage/Footer/SocialProfiles/styled.js
+++ src/features/PersonalHomepage/Footer/SocialProfiles/styled.js
@@ -1,7 +1,23 @@
 import styled from "styled-components";
 
-export const List = styled.ul``;
+export const List = styled.ul`
+    list-style: none;
+    padding:0;
+    display:flex;
+    margin-top:56px;
+    margin-bottom: 0;
+`;
 
-export const Item = styled.li``;
+export const Item = styled.li`
+    margin: 0 18px 0 0;
+`;
+ 
 
-export const Link = styled.a``;
+export const Link = styled.a`
+    color: ${({theme}) => theme.colors.text};
+    transition: color 0.5s;
+
+    &:hover {
+        color: ${({theme}) => theme.colors.primary};
+ }
+`;
