9884c7661a3353b4b52713abfe5eaf967c4fae92
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-08-01 00:51:01 +0200

    Added rootSaga to store

--- src/App/store.js
+++ src/App/store.js
@@ -1,12 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from 'redux-saga';
 import themeReducer from "../common/themeSlice";
 import projectsReducer from "../features/personalHomepage/PersonalHomepageSite/Portfolio/personalProjectsSlice";
+import rootSaga from "./rootSaga";
+
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         theme: themeReducer,
         projects: projectsReducer,
     },
+    middleware: () => [sagaMiddleware],
 });
+sagaMiddleware.run(rootSaga);
+
 
 export default store;
