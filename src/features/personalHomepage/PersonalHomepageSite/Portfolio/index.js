9271415b5275b1bda86280ed2fff1017c0df294e
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-08-01 00:20:13 +0200

    Dispatched actions fetchRepositories in component Portfolio

--- src/features/personalHomepage/PersonalHomepageSite/Portfolio/index.js
+++ src/features/personalHomepage/PersonalHomepageSite/Portfolio/index.js
@@ -1,12 +1,24 @@
 import { Header, StyledGitHubIcons, MyProjects, Section } from "./styled";
 import { SubHeader } from "../SubHeader";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchRepositories } from "./personalProjectsSlice";
 
-export const Portfolio = () => (
-    <Section>
-        <Header>
-            <StyledGitHubIcons />
-            <SubHeader>Portfolio</SubHeader>
-            <MyProjects>My recent projects</MyProjects>
-        </Header>
-    </Section>
-)
+export const Portfolio = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchRepositories());
+    }, [dispatch])
+
+    return (
+        <Section>
+            <Header>
+                <StyledGitHubIcons />
+                <SubHeader>Portfolio</SubHeader>
+                <MyProjects>My recent projects</MyProjects>
+            </Header>
+        </Section>
+    )
+}
