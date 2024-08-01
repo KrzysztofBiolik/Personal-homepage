fe6397cd76abf92e2bc16ad4222e83b72ebb9c60
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-08-01 23:26:32 +0200

    Added error handling in personalProjectsSaga

--- src/features/personalHomepage/PersonalHomepageSite/Portfolio/personalProjectsSaga.js
+++ src/features/personalHomepage/PersonalHomepageSite/Portfolio/personalProjectsSaga.js
@@ -1,14 +1,18 @@
 import { call, delay, put, takeLatest } from "redux-saga/effects";
 import { getGithubRepos } from "./GitHubDataAPI";
-import { fetchRepositories, setRepositories } from "./personalProjectsSlice";
+import { fetchRepositories, setError, setRepositories } from "./personalProjectsSlice";
 
 function* fetchRepositoriesHandler() {
-    yield delay(1200);
-    const repositories = yield call(getGithubRepos);
-    yield put(setRepositories(repositories));
-}
+    try {
+        yield delay(1200);
+        const repositories = yield call(getGithubRepos);
+        yield put(setRepositories(repositories));
+    } catch (error) {
+        yield put(setError());
+    }
+};
 
 
 export function* projectsSaga() {
     yield takeLatest(fetchRepositories.type, fetchRepositoriesHandler);
-}
+};
