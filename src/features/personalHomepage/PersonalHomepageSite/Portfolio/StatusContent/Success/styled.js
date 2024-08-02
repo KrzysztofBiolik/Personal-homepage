f44d1049a5f8c4c77014dc7450071148342c995f
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-08-02 13:29:56 +0200

    Used CSS media query to elements in Repositories component

--- src/features/personalHomepage/PersonalHomepageSite/Portfolio/StatusContent/Success/styled.js
+++ src/features/personalHomepage/PersonalHomepageSite/Portfolio/StatusContent/Success/styled.js
@@ -8,26 +8,35 @@ export const List = styled.ul`
     list-style: none;
     padding: 0;
     margin-top: 24px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.tabletVerticalMax}px) {
+        grid-template-columns: 1fr;
+        grid-gap:24px;
+    }
 `;
 
 export const Tile = styled.li`
     background: ${({ theme }) => theme.colors.boxBackground};
     padding: 56px;
     margin: 0;
-    border: 6px solid ${({theme}) => theme.colors.tile.border};
-    box-shadow: ${({theme}) => theme.boxShadow};
-    border-radius: ${({theme}) => theme.borderRadius};
+    border: 6px solid ${({ theme }) => theme.colors.tile.border};
+    box-shadow: ${({ theme }) => theme.boxShadow};
+    border-radius: ${({ theme }) => theme.borderRadius};
     transition: border-color 0.3s;
 
     &:hover {
-        border-color: ${({theme}) => theme.colors.tile.borderHover};
+        border-color: ${({ theme }) => theme.colors.tile.borderHover};
+    }
+
+    @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+        padding: 20px;
     }
 
 `;
 
 export const Name = styled.h3`
     font-size: 24px;
-    color: ${({theme}) => theme.colors.tile.header};
+    color: ${({ theme }) => theme.colors.tile.header};
     margin: 0;
 `;
 
@@ -47,6 +56,10 @@ export const Links = styled.dl`
 export const LinksRow = styled.div`
        display: grid;
        grid-template-columns: 4em 1fr;
+
+       @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+        grid-template-columns: 1fr;
+       }
 `;
 
 export const LinksValue = styled.dd`
@@ -54,8 +67,8 @@ export const LinksValue = styled.dd`
 `;
 
 export const Link = styled.a`
-       color: ${({theme}) => theme.colors.primary};
-       border-bottom: 1px solid ${({theme}) => α(theme.colors.primary, 0.3)};
+       color: ${({ theme }) => theme.colors.primary};
+       border-bottom: 1px solid ${({ theme }) => α(theme.colors.primary, 0.3)};
        text-decoration: none;
        padding-bottom: 1px;
 
